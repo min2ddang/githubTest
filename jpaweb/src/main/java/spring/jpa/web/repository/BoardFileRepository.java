818651a16c523cb1f86c073a0dd551ed3e8b7490
hanmin <lhm0565@naver.com>
2025-05-26 10:35:57 +0900

    Delete jpaweb directory

--- jpaweb/src/main/java/spring/jpa/web/repository/BoardFileRepository.java
+++
@@ -1,9 +0,0 @@
-package spring.jpa.web.repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import spring.jpa.web.entity.BoardFileEntity;
-
-@Repository
-public interface BoardFileRepository extends JpaRepository<BoardFileEntity, Integer> {
-
-}
